f2d3bddc8b8a1d19edf005e4f814b6ad876dbc2f
Md. Foysal <muhammadfoysal2448@gmail.com>
2022-02-25 17:49:01 +0600

    Basic-of-Dart-2nd-day

---
+++ bin/second_day_of_dart.dart
@@ -0,0 +1,67 @@
+// void main(){
+//
+//   int projectWorkersAmount=100;
+//
+//   String projectName='FoysalLTD';
+//
+//   bool projectType=true;
+//
+//   List workersSerial=[1,2,3,];
+//
+//   Map projectLocation={};
+//
+//
+//
+//   var uv='FoysalLTD';
+//
+//
+// }
+
+
+
+// import 'dart:ffi';
+//
+// void main(){  /// Here void is method type, sum is method name;
+//   }
+//
+//         int addition(){
+//           var roll=4;
+//         return roll;
+//
+//           int result=10;
+//           return result;
+//
+//           return 10;
+//         }
+//         double number(){
+//          return 10.0;
+//         }
+//
+//         bool (){
+//         return true;
+//         }
+//         String userName(){
+//         return 'foysal';
+//         }
+//
+//         List userSeriall(){
+//         return[1,2,3];
+//         }
+//
+//         Map area(){
+//         return{'Kye':'Value'};
+//         }
+//
+//
+// Map<String, dynamic> myMapTypeMethod() {
+//
+//   var map ={
+//     'name': 'foysal',
+//     'Id': 10,
+//     'list': ['foysal', 'samir'],
+//     'nullvalue': null
+//   };
+//
+// // return {'kye':'value'};
+// return map;
+// }
